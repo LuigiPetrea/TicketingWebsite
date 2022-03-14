9fc0abeee9d5c12f0d8070e3be805113396b3009
Petrea Levi - Luigi <levipetrea08@gmail.com>
2022-03-14 22:06:26 +0200

    JavaScript

---
+++ main.js
@@ -0,0 +1,18 @@
+function calc(adults, children){ 
+    return adults * 12 + children * 5;
+}
+if (adults < 0){
+    adults = 0;
+}
+if(children < 0){
+    children = 0;
+}
+window.onload = function() {
+  let btn = document.getElementById("buy");
+  btn.onclick = function() {
+    let adults = document.getElementById("adults").value;
+    let children = document.getElementById("children").value;
+    let price = calc(adults, children);
+    alert(price);
+  }
+} 
